18c8d464e84698b002dc010f3875ec68653093ca
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-04-28 09:00:21 -0300

    Login

--- src/services/server.js
+++ src/services/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, {Router} from 'express';
 import MainRouter from '../routes'
 import MongoStore from 'connect-mongo';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import config from '../config';
-
+import passport from 'passport';
+import InstagramStrategy from 'passport-instagram';
 
 const app = express()
 
@@ -33,5 +34,44 @@ app.use(session(StoreOptions));
 const mySecret = 'mySecret';
 app.use(cookieParser(mySecret));
 app.use('/api', MainRouter)
+
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new InstagramStrategy({
+    clientID: 'TU_CLIENT_ID_DE_INSTAGRAM',
+    clientSecret: 'TU_CLIENT_SECRET_DE_INSTAGRAM',
+    callbackURL: 'http://localhost:3000/auth/instagram/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+// Iniciar sesión con Instagram
+app.get('/auth/instagram', passport.authenticate('instagram'));
+
+app.get('/auth/instagram/callback',
+  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+// Protege las rutas que requieren autenticación
+  app.get('/profile', isAuthenticated, function(req, res) {
+    res.render('profile', { user: req.user });
+  });
+// Middleware para verificar si el usuario está autenticado
+  function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/login');
+  }
 
 export default app
