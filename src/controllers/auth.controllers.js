5d0a5ac37f9c1077199dc22f0aa74b3bdf146273
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-05-14 01:42:16 -0300

    Update auth.controllers.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -21,7 +21,7 @@ export const getToken = async (req, res, next) => {
 
     // PRIMERO PEDIMOS EL CODIGO QUE NOS MANDA INSTAGRAM, CUANDO EL USUARIO DA PERMISO DE INSTAGRAM
     const code = req.query.code
-    
+
          // PETICION A API GRAPH INSTAGRAM: ACCESS TOKEN 
     const form = new FormData();
     form.append('client_id', '180895391557997');
@@ -45,6 +45,14 @@ export const getToken = async (req, res, next) => {
     const token = response.data.access_token
 
     res.send(`El access Token es: ${token}`)
+
+    // const userData = await axios.get('https://graph.facebook.com/v16.0/me', {
+    //     params: {
+    //         'fields': 'name',
+    //         'access_token': token
+    //     }
+    //     });
+    // res.send(userData)
     next()
     
 } catch(err) {
@@ -71,32 +79,37 @@ export const getToken = async (req, res, next) => {
 export const getDataIg = async (req, res) => {
 
     try {
-        const code = 'AQBv6xjIhX5bVonjU0O9na5FNDHHEN3IrCKO1tl8EzDZsdSNZs3lOQY3N6pOh6Hm4lETtjZv2G8XYsZ8N0dyq1MIvhqLyL0gFq_0uTirpzY8k0eNDEtqzidUJUOiSV0ae-NeFSlXyO-FoeH3FeshkG34H6B23VjiBb7cwGCzwFIkNsRhjtgJeUr2RJxjrpF5pH6uAD4BXSgX4c6rjmZs6DF3g2f8MTijijyXR_jK6HlIBA'
-         // PETICION A API GRAPH INSTAGRAM: ACCESS TOKEN 
-        const form = new FormData();
-        form.append('client_id', '180895391557997');
-        form.append('client_secret', 'b45df1e98fe84fceb1924f7c451a584e');
-        form.append('grant_type', 'authorization_code');
-        form.append('redirect_uri', 'https://localhost:443/api/auth/code');
-        form.append('code', code);
+        // const code = 'AQBv6xjIhX5bVonjU0O9na5FNDHHEN3IrCKO1tl8EzDZsdSNZs3lOQY3N6pOh6Hm4lETtjZv2G8XYsZ8N0dyq1MIvhqLyL0gFq_0uTirpzY8k0eNDEtqzidUJUOiSV0ae-NeFSlXyO-FoeH3FeshkG34H6B23VjiBb7cwGCzwFIkNsRhjtgJeUr2RJxjrpF5pH6uAD4BXSgX4c6rjmZs6DF3g2f8MTijijyXR_jK6HlIBA'
+        //  // PETICION A API GRAPH INSTAGRAM: ACCESS TOKEN 
+        // const form = new FormData();
+        // form.append('client_id', '180895391557997');
+        // form.append('client_secret', 'b45df1e98fe84fceb1924f7c451a584e');
+        // form.append('grant_type', 'authorization_code');
+        // form.append('redirect_uri', 'https://localhost:443/api/auth/code');
+        // form.append('code', code);
 
 
-        // ACA CON LOS DATOS DE NUESTRA CUENTA Y EL CODE QUE LE DAN AL USUARIO AL INGRESAR CON INSTAGRAM, HACEMOS EL POST QUE NOS TRAE EL ACCESS_TOKEN
-        const response = await axios.post(
-        'https://api.instagram.com/oauth/access_token',
-        form,
-        {
-            headers: {
-            ...form.getHeaders()
-            }
-        }
-        )
-        const token = response.data.access_token
+        // // ACA CON LOS DATOS DE NUESTRA CUENTA Y EL CODE QUE LE DAN AL USUARIO AL INGRESAR CON INSTAGRAM, HACEMOS EL POST QUE NOS TRAE EL ACCESS_TOKEN
+        // const response = await axios.post(
+        // 'https://api.instagram.com/oauth/access_token',
+        // form,
+        // {
+        //     headers: {
+        //     ...form.getHeaders()
+        //     }
+        // }
+        // )
+        // const token = response.data.access_token
             
-        const userDataResponse = await axios.get(`https://api.instagram.com/v1/users/self/?access_token=${token}`);
+        const userDataResponse = await axios.get('https://graph.facebook.com/v16.0/me', {
+            params: {
+                'fields': 'name',
+                'access_token': 'EAAQgnwlIu5kBAIwrpZBsSlOyvSjMtXpZCFMaq83FdIrS2kIqOriT5VOKtdXfm1qqpVkd4Rp2hCADrqeajZAkLvIOHUtVsZAumkzpaanzS7L85KfZCkNI8ZAMRA7rXxzXuZCGc5DPO2B343ZAMTegLNAPRIKdVrzPhiZBpdvP12osudfOdEvHlq28D5R6ZBa9gZAQHpMnpuZBAY8ZC0Pb6CdZBJZCUwp'
+            }
+            });
 
-    const { data: userData } = userDataResponse;
-    res.send(userData);
+        const { data: userData } = userDataResponse;
+        res.send(userData);
 
     } catch(err) {
         res.send(err)
